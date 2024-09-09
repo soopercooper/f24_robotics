066460d1daf259b7138d535ccdd2fbb4a08b12df
Avery Fernandez <amfernandez7@crimson.ua.edu>
2024-09-09 14:07:41 -0500

    chore: Update obstacle avoidance behavior and stall detection

--- webots_ros2_homework1_python/webots_ros2_homework1_python/webots_ros2_homework1_python.py
+++ webots_ros2_homework1_python/webots_ros2_homework1_python/webots_ros2_homework1_python.py
@@ -13,9 +13,9 @@ import math
 
 # Constants defining robot behavior and safety thresholds
 LINEAR_VEL = 0.22  # Default linear velocity of the robot when moving forward
-STOP_DISTANCE = .5  # Minimum distance to an obstacle before stopping, default 0.2 meters
+STOP_DISTANCE = 0.4  # Minimum distance to an obstacle before stopping, default 0.2 meters
 LIDAR_ERROR = 0.05  # Error margin for LIDAR readings
-LIDAR_AVOID_DISTANCE = 0.9  # Distance at which the robot should start avoiding an obstacle, default 0.5 meters
+LIDAR_AVOID_DISTANCE = .7  # Distance at which the robot should start avoiding an obstacle, default 0.5 meters
 SAFE_STOP_DISTANCE = STOP_DISTANCE + LIDAR_ERROR  # Safe distance to avoid a collision
 # Indices of the LIDAR scan array corresponding to different directions relative to the robot
 RIGHT_SIDE_INDEX = 270
@@ -55,6 +55,8 @@ class RandomWalk(Node):
         self.laser_forward = 0  # Variable to store forward LIDAR data
         self.odom_data = 0  # Variable to store odometry data
         self.pose_saved = ''  # Variable to save the robot's position
+        self.rolling_past_distance = []  # List to store past positions for stall detection
+        self.store_ticker = 0  # Variable to store the current tick count
         self.cmd = Twist()  # Twist message used to control the robot's velocity
         
         # Create a timer to call the timer_callback function periodically (every 0.5 seconds)
@@ -88,10 +90,30 @@ class RandomWalk(Node):
         orientation = msg2.pose.pose.orientation
         
         # Print the robot's current position for debugging purposes
-        self.get_logger().info('self position: {},{},{}'.format(position.x, position.y, position.z))
-        
+        # self.get_logger().info('self position: {},{},{}'.format(position.x, position.y, position.z))
+
+        if self.pose_saved != '' and len(self.rolling_past_distance) >= 10:
+            # Calculate the Euclidean distance between the current and previous positions
+            average_distance = sum(self.rolling_past_distance) / len(self.rolling_past_distance)
+
+            self.get_logger().info('Average distance: %f' % average_distance)
+
+            # Check if the robot has stalled by comparing the current and previous positions
+            if average_distance < 0.01:
+                self.stall = True
+                self.rolling_past_distance = []
+                self.get_logger().info('Stall detected')
+            else:
+                self.stall = False
+
         # Save the current position for potential use in detecting stalls or other purposes
+        if self.store_ticker % 100 == 0 and self.pose_saved != '':
+            self.rolling_past_distance.append(math.sqrt((position.x - self.pose_saved.x)**2 + (position.y - self.pose_saved.y)**2))
+            self.store_ticker = 0
+        if len(self.rolling_past_distance) > 100:
+            self.rolling_past_distance.pop(0)
         self.pose_saved = position
+        self.store_ticker += 1
 
     def timer_callback(self):
         """Callback function to control robot movement based on sensor data."""
@@ -107,44 +129,66 @@ class RandomWalk(Node):
 
         # Check if there's an obstacle within the safe stopping distance
         if front_lidar_min < SAFE_STOP_DISTANCE:
-            if self.turtlebot_moving:
-                # Stop the robot if it's currently moving and an obstacle is too close
-                self.cmd.linear.x = 0.0
-                self.cmd.angular.z = 0.0
-                self.publisher_.publish(self.cmd)
-                self.turtlebot_moving = False
-                self.get_logger().info('Stopping')
-                return
+            # if self.stall:
+            self.cmd.linear.x = 0.0
+            self.cmd.angular.z = 0.3
+            self.publisher_.publish(self.cmd)
+            self.get_logger().info('Rotating')
+            return
+            #     return
+            # if self.turtlebot_moving:
+            #     # Stop the robot if it's currently moving and an obstacle is too close
+            #     self.cmd.linear.x = 0.0
+            #     self.cmd.angular.z = 0.0
+            #     self.publisher_.publish(self.cmd)
+            #     self.turtlebot_moving = False
+            #     self.get_logger().info('Stopping')
+            #     return
         
         # Check if the robot should start avoiding an obstacle
         elif front_lidar_min < LIDAR_AVOID_DISTANCE:
             # Slow down and turn in the direction with more space
             self.cmd.linear.x = 0.07
+            text_direction = None
             if right_lidar_min > left_lidar_min:
                 self.cmd.angular.z = -0.3  # Turn right
-            else:
-                self.cmd.angular.z = 0.3  # Turn left
+                text_direction = 'right'
+            # else:
+            #     self.cmd.angular.z = 0.3  # Turn left
+            #     text_direction = 'left'
             self.publisher_.publish(self.cmd)
-            self.get_logger().info('Turning')
+            self.get_logger().info('Turning %s' % text_direction)
             self.turtlebot_moving = True
         
         # If there's no immediate obstacle, move forward at the normal speed
         else:
             self.cmd.linear.x = 0.3
             self.cmd.angular.z = 0.0
+            # Check if the right wall is too far away
+            if right_lidar_min > 1.0:
+                self.cmd.angular.z = -0.3
+                # Move closer to follow the wall
+                self.cmd.linear.x = 0.1
+                self.get_logger().info('Moving closer to the wall')
+            # Check if the right wall is too close
+            elif right_lidar_min < 0.5:
+                self.cmd.angular.z = 0.3
+                # Move away from the wall
+                self.cmd.linear.x = 0.1
+                self.get_logger().info('Moving away from the wall')
+
             self.publisher_.publish(self.cmd)
             self.turtlebot_moving = True
         
         # Log the distance to the nearest obstacle and the robot's current odometry data
-        self.get_logger().info('Distance of the obstacle: %f' % front_lidar_min)
-        self.get_logger().info('I receive: "%s"' % str(self.odom_data))
+        self.get_logger().info('Distance of the obstacle: %f and I receive: "%s". Average distance: %f' % (front_lidar_min, self.odom_data, self.store_ticker))
         
         # Log a message if the robot is stalled (if stall detection is implemented)
         if self.stall:
             self.get_logger().info('Stall reported')
         
         # Log the current command being sent to the robot
-        self.get_logger().info('Publishing: "%s"' % self.cmd)
+        # self.get_logger().info('Publishing: "%s"' % self.cmd)
 
 def main(args=None):
     # Initialize the ROS communication
