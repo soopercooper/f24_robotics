f0580ca586f1eec744d58178448acefb7ca61a86
Avery Fernandez <amfernandez7@crimson.ua.edu>
2024-09-09 15:01:08 -0500

    chore: Update obstacle avoidance behavior and stall detection

--- webots_ros2_homework1_python/webots_ros2_homework1_python/webots_ros2_homework1_python.py
+++ webots_ros2_homework1_python/webots_ros2_homework1_python/webots_ros2_homework1_python.py
@@ -22,6 +22,10 @@ RIGHT_SIDE_INDEX = 270
 RIGHT_FRONT_INDEX = 210
 LEFT_FRONT_INDEX = 150
 LEFT_SIDE_INDEX = 90
+MAX_ROTATION_SPEED = 0.5  # Maximum angular velocity of the robot when turning
+NO_ROTATION_SPEED = 0.0  # Angular velocity of the robot when not turning
+MAX_LINEAR_SPEED = 0.2  # Maximum linear velocity of the robot when moving forward
+NO_LINEAR_SPEED = 0.0  # Linear velocity of the robot when not moving forward
 
 class RandomWalk(Node):
 
@@ -92,28 +96,28 @@ class RandomWalk(Node):
         # Print the robot's current position for debugging purposes
         # self.get_logger().info('self position: {},{},{}'.format(position.x, position.y, position.z))
 
-        if self.pose_saved != '' and len(self.rolling_past_distance) >= 10:
-            # Calculate the Euclidean distance between the current and previous positions
-            average_distance = sum(self.rolling_past_distance) / len(self.rolling_past_distance)
+        # if self.pose_saved != '' and len(self.rolling_past_distance) >= 10:
+        #     # Calculate the Euclidean distance between the current and previous positions
+        #     average_distance = sum(self.rolling_past_distance) / len(self.rolling_past_distance)
 
-            self.get_logger().info('Average distance: %f' % average_distance)
+        #     self.get_logger().info('Average distance: %f' % average_distance)
 
-            # Check if the robot has stalled by comparing the current and previous positions
-            if average_distance < 0.01:
-                self.stall = True
-                self.rolling_past_distance = []
-                self.get_logger().info('Stall detected')
-            else:
-                self.stall = False
+        #     # Check if the robot has stalled by comparing the current and previous positions
+        #     if average_distance < 0.01:
+        #         self.stall = True
+        #         self.rolling_past_distance = []
+        #         self.get_logger().info('Stall detected')
+        #     else:
+        #         self.stall = False
 
-        # Save the current position for potential use in detecting stalls or other purposes
-        if self.store_ticker % 100 == 0 and self.pose_saved != '':
-            self.rolling_past_distance.append(math.sqrt((position.x - self.pose_saved.x)**2 + (position.y - self.pose_saved.y)**2))
-            self.store_ticker = 0
-        if len(self.rolling_past_distance) > 100:
-            self.rolling_past_distance.pop(0)
+        # # Save the current position for potential use in detecting stalls or other purposes
+        # if self.store_ticker % 100 == 0 and self.pose_saved != '':
+        #     self.rolling_past_distance.append(math.sqrt((position.x - self.pose_saved.x)**2 + (position.y - self.pose_saved.y)**2))
+        #     self.store_ticker = 0
+        # if len(self.rolling_past_distance) > 100:
+        #     self.rolling_past_distance.pop(0)
         self.pose_saved = position
-        self.store_ticker += 1
+        # self.store_ticker += 1
 
     def timer_callback(self):
         """Callback function to control robot movement based on sensor data."""
@@ -130,8 +134,8 @@ class RandomWalk(Node):
         # Check if there's an obstacle within the safe stopping distance
         if front_lidar_min < SAFE_STOP_DISTANCE:
             # if self.stall:
-            self.cmd.linear.x = 0.0
-            self.cmd.angular.z = 0.3
+            self.cmd.linear.x = NO_LINEAR_SPEED
+            self.cmd.angular.z = MAX_ROTATION_SPEED
             self.publisher_.publish(self.cmd)
             self.get_logger().info('Rotating')
             return
@@ -148,10 +152,10 @@ class RandomWalk(Node):
         # Check if the robot should start avoiding an obstacle
         elif front_lidar_min < LIDAR_AVOID_DISTANCE:
             # Slow down and turn in the direction with more space
-            self.cmd.linear.x = 0.07
+            self.cmd.linear.x = MAX_LINEAR_SPEED / 4
             text_direction = None
             if right_lidar_min > left_lidar_min:
-                self.cmd.angular.z = -0.3  # Turn right
+                self.cmd.angular.z = -MAX_ROTATION_SPEED  # Turn right
                 text_direction = 'right'
             # else:
             #     self.cmd.angular.z = 0.3  # Turn left
@@ -162,26 +166,33 @@ class RandomWalk(Node):
         
         # If there's no immediate obstacle, move forward at the normal speed
         else:
-            self.cmd.linear.x = 0.3
-            self.cmd.angular.z = 0.0
+            self.cmd.linear.x = MAX_LINEAR_SPEED
+            self.cmd.angular.z = NO_ROTATION_SPEED
             # Check if the right wall is too far away
             if right_lidar_min > 1.0:
-                self.cmd.angular.z = -0.3
+                self.cmd.angular.z = -MAX_ROTATION_SPEED
                 # Move closer to follow the wall
-                self.cmd.linear.x = 0.1
+                self.cmd.linear.x = MAX_LINEAR_SPEED / 2
                 self.get_logger().info('Moving closer to the wall')
             # Check if the right wall is too close
-            elif right_lidar_min < 0.5:
-                self.cmd.angular.z = 0.3
+            elif right_lidar_min < 0.3:
+                self.cmd.angular.z = MAX_ROTATION_SPEED
                 # Move away from the wall
-                self.cmd.linear.x = 0.1
+                self.cmd.linear.x = MAX_LINEAR_SPEED / 2
                 self.get_logger().info('Moving away from the wall')
+            # If the right wall is within the acceptable range, adjust angular speed smoothly
+            elif right_lidar_min > 0.5:
+                # Apply a cubic function to smoothly reduce angular speed
+                # Scale the rotation speed based on the distance to the wall (between 0.3 and 1.0)
+                proportional_speed = (right_lidar_min - 0.5) / (1.0 - 0.5)  # Normalize between 0 and 1
+                self.cmd.angular.z = -MAX_ROTATION_SPEED / 2 * (1 - proportional_speed ** 3)
+                self.get_logger().info(f'Adjusting rotation, angular.z = {self.cmd.angular.z}')
 
             self.publisher_.publish(self.cmd)
             self.turtlebot_moving = True
         
         # Log the distance to the nearest obstacle and the robot's current odometry data
-        self.get_logger().info('Distance of the obstacle: %f and I receive: "%s". Average distance: %f' % (front_lidar_min, self.odom_data, self.store_ticker))
+        self.get_logger().info('Distance of the obstacle: %f and I receive: "%s". Distance to the right wall: %f' % (front_lidar_min, self.odom_data, right_lidar_min))
         
         # Log a message if the robot is stalled (if stall detection is implemented)
         if self.stall:
